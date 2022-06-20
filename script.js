e32f3043df9c944163e552e3d4f7055f68bfd65e
Maciel-Dev <jvmacielv@gmail.com>
2022-06-20 17:42:42 -0300

    Cont

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const login = document.querySelector("#submitButton");
-let loaded = 0;
+const nameH2 = document.querySelector("#welcome");
+let loaded = 0; 
+let numb = 0;
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    loaded = 1;
     console.log("Olá, eu existo");
+    // Comentário para caso o botao não funcione mais -> Evento de carregamento principal da página
 });
 
-if(loaded == 1){  
-    login.addEventListener('mouseover', e =>{
-        console.log(e)
-    })
-};
+
+login.addEventListener("click", e=>{ //Botão de login
+    numb++;
+    document.querySelector("#contagem").textContent = numb;
+})
+
+nameH2.addEventListener("mouseover", e=>{
+    nameH2.classList.add("movementLeft");
+})
 
